18d53f024c1fd8d5ed78647671e5b8975ba70def
Daniel DiLillo <DDiLillo@midmark.com>
2021-04-02 11:22:34 -0400

    updates

--- WorldTrackerWeb/Controllers/VisitsController.cs
+++ WorldTrackerWeb/Controllers/VisitsController.cs
@@ -1,16 +1,79 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using WorldTrackerDomain.Commands;
+using WorldTrackerDomain.Queries;
+using WorldTrackerWeb.Models;
 
 namespace WorldTrackerWeb.Controllers
 {
     public class VisitsController : Controller
     {
-        public IActionResult Index()
+        private readonly IMediator _mediator;
+
+        public VisitsController(IMediator mediator)
         {
-            return View();
+            _mediator = mediator;
+        }
+
+        public async Task<ActionResult> Create()
+        {
+            var model = new VisitCreateViewModel();
+
+            var personGetAllQueryResult = await _mediator.Send(new PersonGetAllQuery());
+
+            model.People = personGetAllQueryResult.People.Select(i => new SelectListItem(i.Name, i.ID)).ToList();
+
+            model.PersonID = model.People.FirstOrDefault()?.Value;
+
+            var placeGetAllQueryResult = await _mediator.Send(new PlaceGetAllQuery());
+
+            model.Places = placeGetAllQueryResult.Places.Select(i => new SelectListItem(i.Name, i.ID)).ToList();
+
+            model.PlaceID = model.Places.FirstOrDefault()?.Value;
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [RequestFormLimits(MultipartBodyLengthLimit = 209715200)]
+        public async Task<ActionResult> Create(VisitCreateViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            try
+            {
+                var cmd = new VisitCreateCommand
+                {
+                    PersonID = model.PersonID,
+                    PlaceID = model.PlaceID
+                };
+
+                await _mediator.Send(cmd);
+
+                return RedirectToAction("Index", "Home");
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+
+                var personGetAllQueryResult = await _mediator.Send(new PersonGetAllQuery());
+
+                model.People = personGetAllQueryResult.People.Select(i => new SelectListItem(i.Name, i.ID)).ToList();
+
+                var placeGetAllQueryResult = await _mediator.Send(new PlaceGetAllQuery());
+
+                model.Places = placeGetAllQueryResult.Places.Select(i => new SelectListItem(i.Name, i.ID)).ToList();
+
+                return View(model);
+            }
         }
     }
 }
--- WorldTrackerWeb/Models/VisitCreateViewModel.cs
+++ WorldTrackerWeb/Models/VisitCreateViewModel.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace WorldTrackerWeb.Models
 {
-    public class VisitViewModel
+    public class VisitCreateViewModel
     {
+        public List<SelectListItem> People { get; set; }
 
+        [Required]
+        [MaxLength(50)]
+        [Display(Name ="Person")]
+        public string PersonID { get; set; }
+
+        public List<SelectListItem> Places { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [Display(Name = "Place")]
+        public string PlaceID { get; set; }
     }
 }
